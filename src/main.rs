6fd1ab54cb3684e03a7546da7dc4fe2f2d30c00a
algasami <105358814+algasami@users.noreply.github.com>
2024-02-20 20:35:20 +0800

    added engine + test file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use boson_rust::linalg::{Vec4, ID_MAT4X4};
+use boson_rust::engine::BosonEngine;
 fn main() {
     println!("Hello, world!");
-    let v = Vec4 {
-        data: [1.0, 2.0, 3.0, 1.0],
-    };
-    let v_new: Vec4 = ID_MAT4X4 / 4.0 * v;
-    println!("{}", v_new);
-    println!("{}", ID_MAT4X4);
+    let engine: BosonEngine<100, 20> = Default::default();
+    engine.display();
 }
