615306cf986044f57a697896fac56e4605f3785c
algasami <105358814+algasami@users.noreply.github.com>
2024-02-22 16:39:41 +0800

    mul test

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,9 @@ mod tests {
 
     #[test]
     fn linalg_vec4_add() {
-        let v0 = linalg::Vec3 { data: [0.0; 4] };
+        let v0 = linalg::Vec3 { data: [0.0; 3] };
         let v1 = linalg::Vec3 {
-            data: [0.0, 1.0, 2.0, 1.0],
+            data: [0.0, 1.0, 2.0],
         };
         assert_eq!(v0 + v1, v1);
     }
@@ -17,30 +17,43 @@ mod tests {
     #[test]
     fn linalg_mat4x4_vec4_mul() {
         let v0 = linalg::Vec3 {
-            data: [0.0, 1.0, 2.0, 1.0],
+            data: [0.0, 1.0, 1.0],
         };
-        assert_eq!(linalg::ID_MAT4X4 * 2.0 * v0, v0 * 2.0);
+        let mat = linalg::Mat4x4 {
+            data: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 2.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+        };
+        assert_eq!(
+            mat * v0,
+            linalg::Vec3 {
+                data: [0.0, 1.0, 3.0]
+            }
+        );
     }
     #[test]
     fn linalg_vec4_mag() {
         let v0 = linalg::Vec3 {
-            data: [3.0, 4.0, 0.0, 1.0],
+            data: [3.0, 4.0, 0.0],
         };
         assert_eq!(v0.get_magnitude(), 5.0);
     }
     #[test]
     fn linalg_checkinside() {
         let v0 = linalg::Vec3 {
-            data: [0.0, 0.0, 0.0, 1.0],
+            data: [0.0, 0.0, 0.0],
         };
         let v1 = linalg::Vec3 {
-            data: [3.0, 0.0, 0.0, 1.0],
+            data: [3.0, 0.0, 0.0],
         };
         let v2 = linalg::Vec3 {
-            data: [0.0, 4.0, 0.0, 1.0],
+            data: [0.0, 4.0, 0.0],
         };
         let p = linalg::Vec3 {
-            data: [0.5, 0.5, 0.0, 1.0],
+            data: [0.5, 0.5, 0.0],
         };
         assert!(linalg::check_inside(&v0, &v1, &v2, &p));
     }
